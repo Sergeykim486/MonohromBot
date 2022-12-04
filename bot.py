c4df7278d14c403e48e5a8c49ae05c564fe7a282
Sergey Kim <Sergeykim486@gmail.com>
2022-12-04 23:47:39 +0500

    user registration and sendtoall function

--- bot.py
+++ bot.py
@@ -1,66 +1,178 @@
 import telebot
 from telebot import types
 import json
+from datetime import datetime, date, time
 bot = telebot.TeleBot('5750687344:AAEVWNtI3WGoalFjtuSqWXX1SxxWNmvhNnw')
-
+# Глобальные переменные
 uid = 0
 ufname = ''
 ulname = ''
 uphone = ''
 ustat = ''
 finded = 0
-
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 a = telebot.types.ReplyKeyboardRemove()
 
 @bot.message_handler(commands=['start'])
 
+# ====================  С Т А Р Т   Б О Т А  ====================
 def send_welcome(message):
     global uid, ufname, ulname, uphone, ustat, finded
     markup = types.ReplyKeyboardRemove()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.row('Начать работу.')
+    markup.row('Начать работу')
     bot.send_message(message.chat.id, 'Добро пожаловать в систему.', reply_markup=markup)
     bot.register_next_step_handler(message, start_working)
 def start_working(message):
-    global uid, ufname, ulname, uphone, ustat
     markup = types.ReplyKeyboardRemove()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    usr = open('Users.txt', 'r')
-    usrt = usr.read()
-    usrj = json.loads(usrt)
-    for users in usrj['response']['users']:
-        if int(message.chat.id) == int(users['user_id']):
-            finded = 1
-            uid = users['user_id']
-            ufname = users['user_f_name']
-            ulname = users['user_l_name']
-            uphone = users['user_phone_num']
-            ustat = users['user_status']
-            bot.send_message(message.chat.id, f'Приветствую {ufname}!\nВы авторизованы как {ustat}.')
-            if ustat == 'Manager':
-                markup.row('Список заявок', 'Добавить заявку')
-                bot.send_message(message.chat.id, 'Выберите действие...', reply_markup=markup)
-                bot.register_next_step_handler(message, main_menu_manager)
-            elif ustat == 'Master':
-                markup.row('Список заявок')
-                bot.send_message(message.chat.id, 'Выберите действие...', reply_markup=markup)
-                bot.register_next_step_handler(message, main_menu_master)
-    if finded == 0:
-        markup.row('Зарегистрироваться')
-        bot.send_message(message.chat.id, f'Приветствую!\nВы не авторизованы.\nПожалуйста зарегистрируйтесь.', reply_markup=markup)
-        bot.register_next_step_handler(message, reg1)
-    usr.close()
+    markup.row('Начать работу')
+    if message.text == 'Начать работу' or message.text == 'Главное меню':
+        global uid, ufname, ulname, uphone, ustat
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        usr = open('Users.txt', 'r')
+        usrt = usr.read()
+        usrj = json.loads(usrt)
+        finded = 0
+        for users in usrj['response']['users']:
+            if int(message.chat.id) == int(users['user_id']):
+                finded = 1
+                uid = users['user_id']
+                ufname = users['user_f_name']
+                ulname = users['user_l_name']
+                uphone = users['user_phone_num']
+                ustat = users['user_status']
+                bot.send_message(message.chat.id, f'Приветствую {ufname}!\nВы авторизованы как {ustat}.')
+                if ustat == 'Manager':
+                    markup.row('Список заявок', 'Добавить заявку')
+                    markup.row('Написать всем участникам')
+                    bot.send_message(message.chat.id, 'Выберите действие...', reply_markup=markup)
+                    bot.register_next_step_handler(message, main_menu_manager)
+                elif ustat == 'Master':
+                    markup.row('Список заявок', 'Написать всем участникам')
+                    bot.send_message(message.chat.id, 'Выберите действие...', reply_markup=markup)
+                    bot.register_next_step_handler(message, main_menu_master)
+        if finded == 0:
+            markup.row('Зарегистрироваться')
+            bot.send_message(message.chat.id, f'Приветствую!\nВы не авторизованы.\nПожалуйста зарегистрируйтесь.', reply_markup=markup)
+            bot.register_next_step_handler(message, reg1)
+        usr.close()
+    else:
+        bot.send_message(message.chat.id, 'Команда не распознана!', reply_markup=markup)
+        bot.register_next_step_handler(message, start_working)
+# ===============================================================
 
 @bot.message_handler(content_types=['text'])
 
+# ==================== ГЛАВНОЕ МЕНЮ ПОЛЬЗОВАТЕЛЯ ====================
 def main_menu_manager(Message):
-    bot.send_message(Message.chat.id, '🟦🟨приступим', reply_markup=a)
-
+    if Message.text == 'Список заявок':
+        now = datetime.now()
+        tasksfile = str(now.month) + str(now.year) + '.txt'
+        # bot.send_message(Message.chat.id, tasksfile)
+        file = open(tasksfile, 'r')
+        tasklist = json.loads(file.read())
+        for tasks in tasklist['response']['tasks']:
+            tnum = tasks['task_id']
+            tdate = tasks['task_add_date']
+            ttime = tasks['task_add_time']
+            tfrom = tasks['task_from']
+            tmaster = tasks['master_name']
+            tmanagerc = tasks['manager_name_c']
+            tstatus = tasks['task_status']
+            if tstatus == 1:
+                taskmes = '🔵' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom)
+            elif tstatus == 2:
+                taskmes = '🟡' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom) + ' (' + str(tmaster) + ')'
+            elif tstatus == 3:
+                taskmes = '🟢' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom) + ' (' + str(tmaster) + ')'
+            elif tstatus == 4:
+                taskmes = '🔴' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom) + ' (' + str(tmanagerc) + ')'
+            bot.send_message(Message.chat.id, taskmes)
+        bot.register_next_step_handler(Message, main_menu_manager)
+    elif Message.text == 'Написать всем участникам':
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('Отмена')
+        bot.send_message(Message.chat.id, 'Что вы хотите написать?', reply_markup=markup)
+        bot.register_next_step_handler(Message, chat)
+    else:
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('Список заявок', 'Добавить заявку')
+        markup.row('Написать всем участникам')
+        bot.send_message(Message.chat.id, 'команда не опознана.', reply_markup=markup)
+        bot.register_next_step_handler(Message, main_menu_manager)
 def main_menu_master(Message):
-    bot.send_message(Message.chat.id, '🟦🟨🟩🟥приступим', reply_markup=a)
+    if Message.text == 'Список заявок':
+        now = datetime.now()
+        tasksfile = str(now.month) + str(now.year) + '.txt'
+        # bot.send_message(Message.chat.id, tasksfile)
+        file = open(tasksfile, 'r')
+        tasklist = json.loads(file.read())
+        for tasks in tasklist['response']['tasks']:
+            tnum = tasks['task_id']
+            tdate = tasks['task_add_date']
+            ttime = tasks['task_add_time']
+            tfrom = tasks['task_from']
+            tmaster = tasks['master_name']
+            tmanagerc = tasks['manager_name_c']
+            tstatus = tasks['task_status']
+            if tstatus == 1:
+                taskmes = '🔵' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom)
+            elif tstatus == 2:
+                taskmes = '🟡' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom) + ' (' + str(tmaster) + ')'
+            elif tstatus == 3:
+                taskmes = '🟢' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom) + ' (' + str(tmaster) + ')'
+            elif tstatus == 4:
+                taskmes = '🔴' + str(tdate) + ' ' + str(ttime) + ' - ' + str(tfrom) + ' (' + str(tmanagerc) + ')'
+            bot.send_message(Message.chat.id, taskmes)
+        bot.register_next_step_handler(Message, main_menu_master)
+    elif Message.text == 'Написать всем участникам':
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('Отмена')
+        bot.send_message(Message.chat.id, 'Что вы хотите написать?', reply_markup=markup)
+        bot.register_next_step_handler(Message, chat)
+    else:
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('Список заявок', 'Написать всем участникам')
+        bot.send_message(Message.chat.id, 'команда не опознана.', reply_markup=markup)
+        bot.register_next_step_handler(Message, main_menu_master)
+# ===================================================================
 
-# РЕГИСТРАЦИЯ ПОЛЬЗОВАТЕЛЯ
+# ==================== отправка сообщения всем ====================
+def chat(message):
+    if message.text != 'Отмена':
+        sfname = ''
+        slname = ''
+        usr = open('Users.txt', 'r')
+        usrt = usr.read()
+        usrj = json.loads(usrt)
+        for users in usrj['response']['users']:
+            if int(message.chat.id) == int(users['user_id']):
+                sfname = users['user_f_name']
+                slname = users['user_l_name']
+        for users in usrj['response']['users']:
+            if int(message.chat.id) != int(users['user_id']):
+                bot.send_message(users['user_id'], f"‼️ сообщение ‼️ \n от {slname} {sfname} ‼️\n\n{message.text}")
+        usr.close()
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('Главное меню')
+        bot.send_message(message.chat.id, 'Ваше сообщение отправлено', reply_markup=markup)
+        bot.register_next_step_handler(message, start_working)
+    else:
+        markup = types.ReplyKeyboardRemove()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.row('Главное меню')
+        bot.send_message(message.chat.id, 'Возврат в главное меню.', reply_markup=markup)
+        bot.register_next_step_handler(message, start_working)
+# =================================================================
+
+# ==================== РЕГИСТРАЦИЯ НОВОГО ПОЛЬЗОВАТЕЛЯ ====================
 def reg1(Message):
     markup = types.ReplyKeyboardRemove()
     if Message.chat.type == 'private':
@@ -138,11 +250,21 @@ def reg6(message):
             usr = open('Users.txt', 'r')
             usrt = usr.read()
             usrj = json.loads(usrt)
-            usrj['response']['users'] = {'user_id': uid, 'user_f_name': ufname, 'user_l_name': ulname, 'user_phone_num': uphone, 'user_status': ustat}
-            # for users in usrj['response']['users']:
-            #     bot.send_message(message.chat.id, type(users))
-            # usr.close()
-            # usr = open('Users.txt', 'w')
+            json_data = {
+                'user_id': uid,
+                'user_f_name': ufname,
+                'user_l_name': ulname,
+                'user_phone_num': uphone,
+                'user_status': ustat
+                }
+            usrj['response']['users'].append(json_data)
+            for users in usrj['response']['users']:
+                bot.send_message(message.chat.id, users['user_f_name'])
+                bot.send_message(message.chat.id, users['user_l_name'])
+            usr.close()
+            nusr = open('Users.txt', 'w')
+            nusr.write(json.dumps(usrj, indent=2))
+            nusr.close()
             # ======================================
             bot.send_message(message.chat.id, 'Данные сохранены.', reply_markup=markup)
             bot.register_next_step_handler(message, start_working)
@@ -161,6 +283,6 @@ def reg6(message):
                             f'Статус: {ustat}\n\n'
                             f'Все верно?', reply_markup=markup)
             bot.register_next_step_handler(message, reg6)
-# ========================
+# =========================================================================
             
 bot.polling(none_stop=True, interval=0)
